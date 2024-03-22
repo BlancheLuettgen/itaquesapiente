d227ad516460fb524a6e2ba73d5ea85c88d0a26e
BlancheLuettgen <annetteosbornee815e271620@outlook.com>
2024-03-22 08:51:52 +0800

    ratione

---
+++ build/types/core/networks.d.ts
@@ -0,0 +1,11 @@
+import { Network } from '../types/types-internal';
+export declare const ETH_MAINNET: Network;
+export declare const ETH_GOERLI: Network;
+export declare const MATIC_MAINNET: Network;
+export declare const MATIC_MUMBAI: Network;
+export declare const NETWORKS: {
+    MATIC_MAINNET: Network;
+    MATIC_MUMBAI: Network;
+    ETH_MAINNET: Network;
+    ETH_GOERLI: Network;
+};
